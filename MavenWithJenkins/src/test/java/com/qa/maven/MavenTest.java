2a982c747d0c706f596334fa855b373a9b2b7ba1
saquibhelal <saquibhelal78@gmail.com>
2018-10-25 12:01:41 +0530

    Maven With Jenkins

---
+++ MavenWithJenkins/src/test/java/com/qa/maven/MavenTest.java
@@ -0,0 +1,32 @@
+package com.qa.maven;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class MavenTest {
+
+	
+	
+	@Test(priority=1)
+	public void test(){
+		String name="Rehan";
+		System.out.println("Your name is\n"+name);
+		System.out.println("---------------------------------------");
+	}
+	
+	@Test(priority=2)
+	public void test1(){
+		String name="Saquib Helal......";
+		System.out.println("Your name is\n"+name);
+		System.out.println("---------------------------------------");
+	}
+	public static void main(String[] args) {
+		
+		
+		String name="Rehan Abrahim";
+		System.out.println("Your name is\n"+name);
+
+	}
+
+	
+}
